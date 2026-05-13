f428db929aa5990722a94f5bdb8598c0cf48c2c5
MARVER1X <bbmardd@gmail.com>
2026-05-13 17:51:07 +0100

    fix: resolve React hydration error by fixing nested p-tags

--- src/components/common/Signup/termText.tsx
+++ src/components/common/Signup/termText.tsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom';
 const TermText = () => {
   return (
     <div>
-      <p className="text-[#5E6470] text-base flex flex-row gap-3">
-        <p> Already have an account?</p>
-        <Link to="/signIn" className="text-[#035A69]">
+      <div className="text-[#5E6470] text-base flex flex-row gap-3 mb-2">
+        <p>Already have an account?</p>
+        <Link
+          to="/signIn"
+          className="text-[#035A69] font-medium hover:underline"
+        >
           Sign In
         </Link>
-      </p>
+      </div>
 
       <p className=" text-base text-[#91949D]">
         By using Meetminds you agree to the{' '}
